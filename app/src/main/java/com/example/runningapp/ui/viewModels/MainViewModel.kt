fef7f20dc61999ce9c7392b292994b6ebfb59185
Your Name <daryagrichenok@gmail.com>
2023-09-12 19:05:08 +0300

    Add Saving in Database, Setup RecyclerView in RunFragment

--- app/src/main/java/com/example/runningapp/ui/viewModels/MainViewModel.kt
+++ app/src/main/java/com/example/runningapp/ui/viewModels/MainViewModel.kt
@@ -2,10 +2,19 @@ package com.example.runningapp.ui.viewModels
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.runningapp.db.Run
 import com.example.runningapp.repositories.MainRepository
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 class MainViewModel @ViewModelInject constructor(
     val mainRepository: MainRepository
 ): ViewModel() {
+
+    val runsSortedByDate = mainRepository.getAllRunsSortedByDate()
+
+    fun insertRun(run: Run) = viewModelScope.launch {
+        mainRepository.insertRun(run)
+    }
 }
